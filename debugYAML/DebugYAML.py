60bf61015dbaa10e4f0886742816a5deb502229c
Jesse Kyomuhendo <jesse90089@gmail.com>
2026-03-07 19:32:49 +0100

    Updated DebugYAML.py

--- debugYAML/DebugYAML.py
+++ debugYAML/DebugYAML.py
@@ -5,5 +5,5 @@ from settings.SettingsAssistant import CONFIG
 # The PURPOSE of this file is to debug/test config.yaml file
 
 #Retrival test
-print(CONFIG["quick-test"]["message"])
-print(CONFIG["quick-test"]["number"])
+print(CONFIG["debug-yaml"]["message"])
+print(CONFIG["debug-yaml"]["number"])
